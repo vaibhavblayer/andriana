cde1d6d49ea04f73718901ec8fcf2fd4aa142cf9
vaibhavblayer <vaibhavblayer@gmail.com>
2023-02-16 19:44:44 +0530

    updated the update_data functions

--- __main__.py
+++ __main__.py
@@ -13,7 +13,10 @@ from .src.database.fetch_data import fetchdata
 from .src.notes import notes
 from .src.ideas import ideas
 from .src.chatgpt import chatgpt
-#from .src.database import createDatabase
+from .src.database.update_data import updateequation
+from .src.database.update_data import updateproblem
+from .src.database.update_data import updatenotes
+from .src.database.update_data import updateideas
 
 
 
@@ -28,6 +31,10 @@ CONTEXT_SETTINGS = dict(
 def main():
     pass
 
+main.add_command(updateideas)
+main.add_command(updatenotes)
+main.add_command(updateproblem)
+main.add_command(updateequation)
 main.add_command(equation)
 main.add_command(problem)
 main.add_command(instagram)
